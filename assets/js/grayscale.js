57313c3553d00828f0e57a464d4e93276352871e
Valentina Nakayama <31044456+vnakayama@users.noreply.github.com>
2021-04-02 23:24:00 -0300

    minor improvements
    Fix broken icons, added smooth scroll button, improved buttons for mobile view, added my website.

--- assets/js/grayscale.js
+++ assets/js/grayscale.js
@@ -14,6 +14,19 @@
       }
     }
   });
+    
+$('.btn.btn-link.btn-circle[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 48)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
